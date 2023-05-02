7a0d5e34f58e61fecf117684820d7ce9f093f468
mikerazor5786 <mikehok8@gmail.com>
2023-05-01 18:33:11 -0600

    se agregaron datos iportantes para la conexion a la base de datos

--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,9 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 
-		$link = new PDO("mysql:host=localhost;dbname=pos",
-			            "root",
-			            "");
+		$link = new PDO("mysql:host=localhost;dbname=u830864771_posdemo",
+			            "u830864771_mike",
+			            "Mikerazor5786");
 
 		$link->exec("set names utf8");
 
