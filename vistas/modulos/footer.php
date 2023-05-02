1d5ba1322cd9faedf113b51b8577b524dc79454c
mikerazor5786 <mikehok8@gmail.com>
2023-05-01 18:57:15 -0600

    actualizacion del footer.php

--- vistas/modulos/footer.php
+++ vistas/modulos/footer.php
@@ -1,6 +1,6 @@
 <footer class="main-footer">
 	
-	<strong>Copyright &copy; 2023<a href="" target="_blank">Miguel Tellez</a>.</strong>
+	<strong>Copyright &copy; 2023 <a href="" target="_blank">Miguel Tellez</a>.</strong>
 
 	Todos los derechos reservados.
 
